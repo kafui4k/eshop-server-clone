60e18cdcc2aa4272636817066c0720da95151e9d
Kafui Alordo <kalordo7@gmail.com>
2021-10-19 04:15:43 +0000

    Update __id to have id using 'virtuals'

--- models/product.js
+++ models/product.js
@@ -58,5 +58,13 @@ const productSchema = mongoose.Schema({
     }
 });
 
+productSchema.virtual('id').get(function () {
+    return this._id.toHexString();
+});
+
+productSchema.set('toJSON', {
+    virtuals: true,
+});
+
 // model
 exports.Product = mongoose.model('Product', productSchema);
