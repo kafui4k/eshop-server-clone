5ac53a1b8855318343c76b313b5bfe926f8d8ead
Kafui Alordo <kalordo7@gmail.com>
2021-10-19 13:22:53 +0000

    Getting Users count

--- helpers/jwt.js
+++ helpers/jwt.js
@@ -6,15 +6,24 @@ function authJwt() {
 
     return  expressJWT({
         secret,
-        algorithms: ['PS512']
+        algorithms: ['HS256'],
+        isRevoked: isRevoked
     }).unless({
         path: [
-            { url: /\/api\/v1\/products(*)/, methods: ['GET', 'POST']},
-            { url: /\/api\/v1\/categories(*)/, methods: ['GET', 'POST']},
-            `${api}/v1/users/login`,
-            `${api}/v1/users/register`
+            { url: /\/api\/v1\/products(*)/, methods: ['GET', 'OPTIONS']},
+            { url: /\/api\/v1\/categories(*)/, methods: ['GET', 'OPTIONS']},
+            `${api}/users/login`,
+            `${api}/users/register`
         ]
     })
+}
+
+async function isRevoked(req, payload, done) {
+    if (!payload.isAdmin) {
+        done(null, true)
+    }
+    
+    done();
 }
 
 module.exports = authJwt;
